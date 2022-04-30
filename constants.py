26e8562d15dda7d5f85986cbd49eb7edeb1f145b
chrispebble <chrispebble@gmail.com>
2022-04-29 23:43:49 -0700

    fixed mimic3 directory in constants.py for colab settings

--- constants.py
+++ constants.py
@@ -2,12 +2,12 @@ PAD_CHAR = "**PAD**"
 EMBEDDING_SIZE = 100
 MAX_LENGTH = 2500
 
-#where you want to save any models you may train
+# uncomment to run locally
 # MODEL_DIR = '/Users/chrispebble/cs598dl4h/caml-mimic/saved_models/'
 # DATA_DIR = '/Users/chrispebble/cs598dl4h/caml-mimic/mimicdata'
 # MIMIC_3_DIR = '/Users/chrispebble/cs598dl4h/caml-mimic/mimicdata/mimic3'
 
-#colab
+# uncomment to run on colab
 MODEL_DIR = '/content/gdrive/Othercomputers/Pebble-MBP16/mcs/cs598-healthcare/paper-project/code/CS598DL4H/caml-mimic/mimicdata/saved_models/'
 DATA_DIR = '/content/gdrive/Othercomputers/Pebble-MBP16/mcs/cs598-healthcare/paper-project/code/CS598DL4H/caml-mimic/mimicdata'
-MIMIC_3_DIR = '/content/gdrive/Othercomputers/Pebble-MBP16/mcs/cs598-healthcare/paper-project/code/CS598DL4H/caml-mimic/mimicdata'
+MIMIC_3_DIR = '/content/gdrive/Othercomputers/Pebble-MBP16/mcs/cs598-healthcare/paper-project/code/CS598DL4H/caml-mimic/mimicdata/mimic3'
